fa2aefb34c7e674c185fb2248774eb52f2f70a8f
Sebastian Lopez-Cot <slcot@mit.edu>
2017-08-20 23:50:11 -0400

    Basic parsing and repl

---
+++ parsing.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mpc.h"
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "%") == 0) { return x % y; }
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+int main(int argc, char** argv) {
+
+    // Create some parsers
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Parenz = mpc_new("parenz");
+
+    // Define them with the following language
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                       \
+          number    : /-?[0-9]+/ ;                              \
+          operator  : '+' | '-' | '*' | '/' | '%' ;             \
+          expr      : <number> | '(' <operator> <expr>+ ')' ;   \
+          parenz    : /^/ <operator> <expr>+ /$/ ;              \
+        ",
+        Number, Operator, Expr, Parenz);
+
+    puts("Parenz Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while (1) {
+        char* input = readline("parenz> ");
+
+        add_history(input);
+        
+        // Attempt to parse the user input
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Parenz, &r)){
+            // On success print the AST
+            long result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        } else {
+            // error
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
+    }
+
+    mpc_cleanup(4, Number, Operator, Expr, Parenz);
+
+    return 0;
+}
